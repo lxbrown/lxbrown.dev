eac646c4ad8ed51d3c0afd3421b30b694da0b6ce
Alex Brown <lxbrown13@gmail.com>
2020-11-26 19:56:04 -0500

    Add margin around main body. Add dummy text to home page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Portfolio from './portfolio/portfolio';
 import Contact from './contact/contact';
 import Home from './home/home';
 
+import './App.css';
+
 
 export default function App() {
   firebase.analytics().logEvent("page_view", { page_location: "lxbrown.dev", page_path: "/", page_title: "Home" });
@@ -15,21 +17,23 @@ export default function App() {
     <>
       <Header />
 
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route exact path="/portfolio">
-          <Portfolio />
-        </Route>
-        <Route exact path="/contact">
-          <Contact />
-        </Route>
+      <div className="main">
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/portfolio">
+            <Portfolio />
+          </Route>
+          <Route exact path="/contact">
+            <Contact />
+          </Route>
 
-        <Route path="*">
-          <Redirect to="/" />
-        </Route>
-      </Switch>
+          <Route path="*">
+            <Redirect to="/" />
+          </Route>
+        </Switch>
+      </div>
     </>
   );
 }
--- src/home/home.js
+++ src/home/home.js
@@ -4,6 +4,9 @@ import './home.css';
 
 export default function Home() {
     return (
-        <img src={profile} alt="Profile" />
+        <div className="home">
+            <img src={profile} alt="Profile" className="photo"/>
+            <p className="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis mollis aliquam dui in pellentesque. Pellentesque vel euismod eros, ut imperdiet est. Integer eros purus, sollicitudin eu gravida in, suscipit vehicula neque. In ut tincidunt dolor. Fusce vel massa quis massa imperdiet facilisis. Sed molestie porttitor mi, et condimentum nisl tempor sit amet. Sed quis sollicitudin mauris.</p>
+        </div>
     )
 }
