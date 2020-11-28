93efc6ec8d8e972eb620d1f3e2ea487575785cfe
Alex Brown <lxbrown13@gmail.com>
2020-11-27 20:38:25 -0500

    Add framework for portfolio layout. Add a few entries for portfolio

--- TODO.md
+++ TODO.md
@@ -1,7 +1,18 @@
-figure out how to include analytics
-add routing
-add 3 pages - home(landing), work, about
-
+add other icons:
+    perforce
+    typescript
+    objectscript
+    iris
+    firebase
+    c++
+    ...
+update icons:
+    react
+    node
+    express
+add styling to card, flex so that max 2-3 cards fit on each row
+add styling within card, max 4-5 icons per row
+add remaining cards: dbs, source control, etc...
 
 
 
--- src/portfolio/portfolio.js
+++ src/portfolio/portfolio.js
@@ -1,7 +1,80 @@
+import jsIcon from '../assets/icons/javascript.svg';
+import javaIcon from '../assets/icons/java.svg';
+import angularIcon from '../assets/icons/angular.svg';
+import djangoIcon from '../assets/icons/django.svg';
+import expressIcon from '../assets/icons/express.svg';
+import nodeIcon from '../assets/icons/nodejs.svg';
+import pythonIcon from '../assets/icons/python.svg';
+import reactIcon from '../assets/icons/react.svg';
+
+import './portfolio.css';
+
+function Card(props) {
+    const title = props.title
+    const tiles = props.tiles
+
+    return (
+        <div>
+            <h3>{title}</h3>
+            {tiles.map(tile => (
+                <a href={tile.link} target='_blank' rel='noreferrer'>
+                    <img key={tile.name} className='icon' src={tile.icon} alt={tile.name} />
+                </a>
+            ))}
+        </div>
+    )
+}
+
 export default function Portfolio() {
+    const languages = [
+        {
+            name: 'Javascript',
+            icon: jsIcon,
+            link: 'https://www.javascript.com/'
+        },
+        {
+            name: 'Java',
+            icon: javaIcon,
+            link: 'https://www.java.com/'
+        },
+        {
+            name: 'Python',
+            icon: pythonIcon,
+            link: 'https://www.python.org/'
+        }
+    ];
+    const frameworks = [
+        {
+            name: 'Angular',
+            icon: angularIcon,
+            link: 'https://angular.io/'
+        },
+        {
+            name: 'React',
+            icon: reactIcon,
+            link: 'https://reactjs.org/'
+        },
+        {
+            name: 'Django',
+            icon: djangoIcon,
+            link: 'https://www.djangoproject.com/'
+        },
+        {
+            name: 'Express',
+            icon: expressIcon,
+            link: 'https://expressjs.com/'
+        },
+        {
+            name: 'NodeJS',
+            icon: nodeIcon,
+            link: 'https://nodejs.org/'
+        }
+    ]
     return (
         <>
-            <p>Portfolio</p>
+            <h2>Portfolio</h2>
+            <Card title='Languages' tiles={languages} />
+            <Card title='Frameworks' tiles={frameworks} />
         </>
     )
 }
